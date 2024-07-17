2314f10e657b14762c4583058cf1dd892cf829a8
Kevin Wessa <kjwessa@gmail.com>
2024-07-17 19:01:10 -0400

    chore(deps): add eslintrc config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 import MillionLint from "@million/lint";
-import { withContentCollections } from "@content-collections/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
@@ -10,4 +9,4 @@ const withMillionLint = MillionLint.next({
 });
 
 // Export the configuration with Content Collections as the last plugin
-export default withContentCollections(withMillionLint(nextConfig));
+export default withMillionLint(nextConfig);

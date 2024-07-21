35fafe1163e07d2961de447c67142806912365d0
Kevin Wessa <kjwessa@gmail.com>
2024-07-20 20:49:44 -0400

    feat(ui): add FooterNavigation component

---
+++ src/components/FooterNavigation.tsx
@@ -0,0 +1,15 @@
+export const FooterNavigation: React.FC = () => {
+  return (
+    <nav className="my-auto flex gap-5 px-5 py-1">
+      <a href="#about" className="text-sm leading-5">
+        About Us
+      </a>
+      <a href="#meet-fr-jack" className="text-sm leading-5">
+        Meet Fr. Jack
+      </a>
+      <a href="#listen" className="text-sm leading-5">
+        Listen Now
+      </a>
+    </nav>
+  );
+};

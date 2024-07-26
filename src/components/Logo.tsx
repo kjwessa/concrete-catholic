085137c7b366cb1795d74c5c428c22ce3b3bad16
Kevin Wessa <kjwessa@gmail.com>
2024-07-26 14:48:26 -0400

    feat(ui): add initial Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+export function Logo() {
+  return (
+    <div className="flex justify-center items-center">
+      <img 
+        loading="lazy" 
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/7337a8c98f16151ffa623f3b1859c1bf0c5880f75d0c445c2ef5979c35b34dc0?apiKey=30aa5622865a4691b40c2c8f7feb241d&&apiKey=30aa5622865a4691b40c2c8f7feb241d" 
+        alt="Concrete Catholic Logo" 
+        className="aspect-[1.82] w-[175px]" 
+      />
+    </div>
+  );
+}

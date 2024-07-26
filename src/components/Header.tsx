c884dacd0322c179ab331178fee247aa9efff847
Kevin Wessa <kjwessa@gmail.com>
2024-07-26 15:54:36 -0400

    chore(code): prepare header for local images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
-
 import { Logo } from './Logo';
 import { NavItem } from './NavItem';
 
-interface HeaderProps {
-  navItems: Array<{
-    label: string;
-    isActive?: boolean;
-  }>;
-}
 
-export function Header({ navItems }: HeaderProps) {
+ const navItems = [
+    { label: "About Us" },
+    { label: "Meet Fr. Jack" },
+    { label: "Contact" },
+    { label: "Listen Now", isActive: true }
+  ];
+
+
+export function Header() {
   return (
     <header className="flex z-10 justify-center items-center px-16 pt-9 -mt-12 w-full bg-gray-800 max-md:px-5 max-md:max-w-full">
       <div className="flex gap-5 justify-between w-full max-w-[1375px] max-md:flex-wrap max-md:max-w-full">
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
     <div className="flex justify-center items-center">
       <Image 
         loading="lazy" 
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/7337a8c98f16151ffa623f3b1859c1bf0c5880f75d0c445c2ef5979c35b34dc0?apiKey=30aa5622865a4691b40c2c8f7feb241d&&apiKey=30aa5622865a4691b40c2c8f7feb241d" 
+        src="" 
         alt="Concrete Catholic Logo" 
         width={175} 
         height={96}

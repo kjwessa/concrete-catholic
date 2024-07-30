5ca25e83a1f2d38836d88cb8fcaaefe630a236e5
Kevin Wessa <kjwessa@gmail.com>
2024-07-30 12:03:15 -0400

    chore: remove unneeded compressed image files

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,7 +10,7 @@ const navItems = [
 
 export function Header() {
   return (
-    <header className="bg-cc-charcoal z-10 flex w-full items-center justify-center px-16 pt-9 max-md:max-w-full max-md:px-5">
+    <header className="bg-cc-charcoal z-10 flex w-full items-center justify-center px-16 pt-9">
       <div className="flex w-full max-w-[1375px] justify-between gap-5 max-md:max-w-full max-md:flex-wrap">
         <Logo />
         <nav className="my-auto flex items-center text-sm leading-5 text-white text-opacity-50">
--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export function Logo() {
   return (
-    <div className="flex justify-center items-center">
-      <Image 
-        loading="lazy" 
-        src="" 
-        alt="Concrete Catholic Logo" 
-        width={175} 
+    <div className="flex items-center justify-center">
+      <Image
+        loading="lazy"
+        src=""
+        alt="Concrete Catholic Logo"
+        width={175}
         height={96}
-        className="aspect-[1.82] w-[175px]" 
+        className="aspect-[1.82] w-[175px]"
       />
     </div>
   );

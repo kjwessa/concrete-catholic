2e7c465f2102156434e2b3579efa2c27a097a4d9
Kevin Wessa <kjwessa@gmail.com>
2024-07-30 12:22:26 -0400

    feat(ui): improve background color of hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,9 +3,9 @@ import { LinkMain } from "./LinkMain";
 
 export function Hero() {
   return (
-    <section className="w-full self-center bg-gray-800">
-      <div className="container mx-auto w-full pt-24">
-        <div className="grid min-h-screen w-full grid-cols-2 text-sm text-white">
+    <section className="bg-cc-charcoal w-full self-center text-white">
+      <div className="container mx-auto pt-24">
+        <div className="grid min-h-screen grid-cols-2">
           <div className="relative z-20">
             <div className="uppercase text-white/[0.5]" id="div-2">
               A podcast for the Every day Catholic
@@ -16,7 +16,6 @@ export function Hero() {
                 When was the last time you had an encounter with Christ?
               </h1>
             </div>
-
             <p className="mb-8 max-w-[37.50rem] overflow-visible text-lg">
               Encounter begins with a simple prayer. Let us begin.
             </p>
@@ -26,10 +25,7 @@ export function Hero() {
             </div>
           </div>
 
-          <div
-            className="relative flex h-[80vh] w-[40vw] max-w-[46.88rem] flex-col items-center justify-center bg-zinc-700"
-            id="div-3"
-          >
+          <div className="relative flex h-[80vh] w-[40vw] max-w-[46.88rem] flex-col items-center justify-center bg-zinc-700">
             <img
               className="-mb-12 -ml-12 -mr-6 mt-6 inline-block h-full max-h-[56.25rem] w-[37.00rem] max-w-none pb-3 align-middle"
               id="img-1"

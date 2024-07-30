56c2e83f87b9ab5f30aa7a42e2440b88beebde8f
Kevin Wessa <kjwessa@gmail.com>
2024-07-30 12:26:43 -0400

    feat(ui): use Next Image for hero image and center hero text

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,16 +1,17 @@
 import { BtnMain } from "./BtnMain";
 import { LinkMain } from "./LinkMain";
+import Image from "next/image";
+import HeroImage from "@/public/images/Concrete_Catholic-86.jpg";
 
 export function Hero() {
   return (
     <section className="bg-cc-charcoal w-full self-center text-white">
       <div className="container mx-auto pt-24">
-        <div className="grid min-h-screen grid-cols-2">
-          <div className="relative z-20">
+        <div className="grid grid-cols-2">
+          <div className="relative z-20 flex flex-col content-center justify-center">
             <div className="uppercase text-white/[0.5]" id="div-2">
               A podcast for the Every day Catholic
             </div>
-
             <div className="max-w-[47.50rem] overflow-hidden pb-1.5 text-7xl font-extrabold">
               <h1 className="z-20 max-w-[50.00rem] pb-3.5 pt-6">
                 When was the last time you had an encounter with Christ?
@@ -24,14 +25,13 @@ export function Hero() {
               <LinkMain link="/#" text="Meet Fr. Jack Knight" />
             </div>
           </div>
-
           <div className="relative flex h-[80vh] w-[40vw] max-w-[46.88rem] flex-col items-center justify-center bg-zinc-700">
-            <img
-              className="-mb-12 -ml-12 -mr-6 mt-6 inline-block h-full max-h-[56.25rem] w-[37.00rem] max-w-none pb-3 align-middle"
-              id="img-1"
-              src="https://www.concretecatholic.com/images/Concrete_Catholic-86.jpg"
-              srcSet="https://www.concretecatholic.com/images/Concrete_Catholic-86-p-500.jpeg 500w, images/Concrete_Catholic-86.jpg 800w"
-            />
+            <div className="mb-12 ml-12 mr-6 mt-6 inline-block h-full max-h-[56.25rem] w-[37.00rem] max-w-none pb-3 align-middle">
+              <Image
+                src={HeroImage}
+                alt="Fr. Jack Knight sitting in a local brewery."
+              />
+            </div>
           </div>
         </div>
       </div>

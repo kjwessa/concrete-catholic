0dec01f992c4cf50a7380db731eecc87d5a3b225
Kevin Wessa <kjwessa@gmail.com>
2024-07-30 13:29:09 -0400

    feat(ui): migrate ConcreteCard into separate component and add props and types

--- src/app/components/BeConcreteSection.tsx
+++ src/app/components/BeConcreteSection.tsx
@@ -17,47 +17,51 @@ export function BeConcreteSection() {
             </p>
 
             <div className="flex h-auto w-[90%] max-w-[34.38rem] flex-col items-start justify-start self-center overflow-visible">
-              <h3 className="mb-8 text-[2.50rem] font-extrabold leading-none">
-                01. Focus
-              </h3>
-
-              <p className="mb-8 max-w-[44.38rem] text-lg">
-                Keep your eyes fixed on Christ. Literally. Find an image of
-                Jesus, and give yourself time to simply look at Him, and let Him
-                look at you. This simple exercise puts{" "}
-                <em className="italic">everything</em> in perspective, letting
-                Jesus be our focus, and allowing the noise to fade away.
-                <br />
-              </p>
-
-              <h3 className="mb-8 text-[2.50rem] font-extrabold leading-none">
-                02. Pray
-                <br />
-              </h3>
-
-              <p className="mb-8 max-w-[44.38rem] text-lg">
-                This step, while obvious, must still be said. This is a state of
-                "being," not a state of "doing." With your eyes fixed on Christ,
-                it becomes natural to ask Jesus to reveal Himself in the
-                circumstances of your life.
-              </p>
-
-              <h3 className="mb-8 text-[2.50rem] font-extrabold leading-none">
-                03. Anticipate
-                <br />
-              </h3>
-
-              <p className="mb-8 max-w-[44.38rem] text-lg">
-                Expect the unexpected and allow the Lord to enter into your
-                surroundings. Very often, we have a specific idea in our minds
-                how (and where) Christ will come to us. Anticipation is not just
-                about seeking out Christ, but allowing ourselves to be found by
-                Him.{" "}
-              </p>
+              {concreteCardItems.map((card, index) => (
+                <ConcreteCard
+                  key={index}
+                  title={card.title}
+                  content={card.content}
+                />
+              ))}
             </div>
           </div>
         </div>
       </div>
     </section>
+  );
+}
+
+const concreteCardItems = [
+  {
+    title: "01. Focus",
+    content:
+      "Keep your eyes fixed on Christ. Literally. Find an image of Jesus, and give yourself time to simply look at Him, and let Him look at you. This simple exercise puts everything in perspective, letting Jesus be our focus, and allowing the noise to fade away.",
+  },
+  {
+    title: "02. Pray",
+    content:
+      '  This step, while obvious, must still be said. This is a state of "being," not a state of "doing." With your eyes fixed on Christ, it becomes natural to ask Jesus to reveal Himself in the circumstances of your life.',
+  },
+  {
+    title: "03. Anticipate",
+    content:
+      "  Expect the unexpected and allow the Lord to enter into your surroundings. Very often, we have a specific idea in our minds how (and where) Christ will come to us. Anticipation is not just about seeking out Christ, but allowing ourselves to be found by Him.",
+  },
+];
+
+interface ConcreteCardProps {
+  title: string;
+  content: string;
+}
+
+function ConcreteCard({ title, content }) {
+  return (
+    <div>
+      <h3 className="mb-8 text-[2.50rem] font-extrabold leading-none">
+        {title}
+      </h3>
+      <p className="mb-8 max-w-[44.38rem] text-lg">{content}</p>
+    </div>
   );
 }

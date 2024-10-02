a5b743e4a8a4b41ac4439b11442624a4c78c96f5
Kevin Wessa <kjwessa@gmail.com>
2024-10-02 16:53:48 -0400

    refactor: consolidate contact us page into one component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,14 @@
 export function Footer() {
-  return <div>Footer</div>
+  return (
+    <footer className="gap-2.5 self-stretch py-6 w-full text-xs leading-none text-orange-300 bg-pink-800 min-h-[64px] max-md:max-w-full">
+      © 2024 Concrete Catholic. Photography by{' '}
+      <a href="#" className="text-orange-300">
+        Brittney Rivera
+      </a>{' '}
+      + Design by{' '}
+      <a href="#" className="text-orange-300">
+        Brewww
+      </a>
+    </footer>
+  )
 }

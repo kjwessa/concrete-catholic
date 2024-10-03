5cf02e691eae1125c83879b800da1920aaa0d677
Kevin Wessa <kjwessa@gmail.com>
2024-10-02 21:22:52 -0400

    refactor: add FAQ Section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -953,6 +953,84 @@ export default function ConcreteCatholicPage() {
           </div>
         </div>
       </section>
+      <section id="faq-section">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-col items-center justify-center pb-24 pt-24 text-sm">
+            <div className="w-full max-w-3xl">
+              <div className="mb-12 text-center">
+                <h2 className="mb-8 text-4xl font-extrabold md:text-5xl lg:text-6xl">
+                  Got questions?
+                  <br />
+                  We have answers.
+                </h2>
+              </div>
+
+              <div>
+                <div className="mb-6 flex flex-col overflow-hidden rounded-md border-2 border-solid border-yellow-600">
+                  <div className="flex max-w-full cursor-pointer items-center justify-between p-6 text-yellow-600">
+                    <h5 className="mr-10 text-xl font-bold text-black">
+                      What makes Concrete Catholic different from other podcasts?
+                    </h5>
+                    <Image
+                      alt="FAQ Icon"
+                      className="inline-block h-5 w-5 max-w-full align-middle"
+                      src=""
+                    />
+                  </div>
+                  <div className="max-h-0 opacity-0">
+                    <p className="p-6 font-sans text-lg">
+                      Relationship with Jesus Christ will always trump knowledge about Jesus Christ.
+                      While there are a lot of Catholic podcasts out there (many of them great), we
+                      wanted to address the most foundational matter of faith...how to have a
+                      relationship with Jesus Christ.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="mb-6 flex flex-col overflow-hidden rounded-md border-2 border-solid border-yellow-600">
+                  <div className="flex max-w-full cursor-pointer items-center justify-between p-6 text-yellow-600">
+                    <h5 className="mr-10 text-xl font-bold text-black">
+                      Does it matter what order I listen to the episodes?
+                    </h5>
+                    <Image
+                      alt="FAQ Icon"
+                      className="inline-block h-5 w-5 max-w-full align-middle"
+                      src=""
+                    />
+                  </div>
+                  <div className="max-h-0 opacity-0">
+                    <p className="p-6 font-sans text-lg">
+                      Wherever you are joining us in our podcast, it does not matter which order you
+                      listen to the episodes. We have designed Concrete Catholic to be accessible
+                      whether you're listening from the very beginning or joining us on a random
+                      week.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="mb-6 flex flex-col overflow-hidden rounded-md border-2 border-solid border-yellow-600">
+                  <div className="flex max-w-full cursor-pointer items-center justify-between p-6 text-yellow-600">
+                    <h5 className="mr-10 text-xl font-bold text-black">
+                      Where did you learn the prayer you say in the podcast?
+                    </h5>
+                    <Image
+                      alt="FAQ Icon"
+                      className="inline-block h-5 w-5 max-w-full align-middle"
+                      src=""
+                    />
+                  </div>
+                  <div className="max-h-0 opacity-0">
+                    <p className="p-6 font-sans text-lg">
+                      The prayer was really born out of intentional time with Christ around the time
+                      of my ordination (Summer 2018).
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }

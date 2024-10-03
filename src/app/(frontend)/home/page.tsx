72fb82fcabcaa15c4b7c62de7b13dab653cc5add
Kevin Wessa <kjwessa@gmail.com>
2024-10-02 21:15:49 -0400

    refactor: migrate Subscribe component into home page

--- src/app/(frontend)/home/page.tsx
+++ src/app/(frontend)/home/page.tsx
@@ -3,14 +3,13 @@ import { ContactSection } from '../../../components/ContactSection'
 
 import { BeConcreteSection } from '../../../components/BeConcreteSection'
 import { CoverSection } from '../../../components/CoverSection'
-import { SubscribeSection } from '../../../components/SubscribeSection'
+
 import { BioSection } from '../../../components/BioSection'
 import { ListenNowSection } from '../../../components/ListenNowSection'
 
 export default function Page() {
   return (
     <>
-      <SubscribeSection />
       <CoverSection />
       <BeConcreteSection />
 

3382efc8adbc79f309af61933e56735629203211
Kevin Wessa <kjwessa@gmail.com>
2024-10-02 21:11:34 -0400

    chore: remove unneeded WhatIf component

--- src/app/(frontend)/home/page.tsx
+++ src/app/(frontend)/home/page.tsx
@@ -6,7 +6,6 @@ import { BeConcreteSection } from '../../../components/BeConcreteSection'
 import { CoverSection } from '../../../components/CoverSection'
 import { SubscribeSection } from '../../../components/SubscribeSection'
 import { BioSection } from '../../../components/BioSection'
-import { WhatIfSection } from '../../../components/WhatIfSection'
 import { ListenNowSection } from '../../../components/ListenNowSection'
 
 export default function Page() {
@@ -18,7 +17,6 @@ export default function Page() {
       <BeConcreteSection />
 
       <BioSection />
-      <WhatIfSection />
       <ListenNowSection />
       <FAQSection />
       <ContactSection />
